15a684942d28df3e49f8e76d701695291f139824
Chen kejie <ckj493991608@qq.com>
2017-04-22 12:37:39 +0800

    修改itemHold传入参数需求，修改itemHold接口功能

--- app/model/itemHold.php
+++ app/model/itemHold.php
@@ -14,37 +14,46 @@ class itemHold extends Model
     public function getTheDetail($post){
         if (!isset($post['DiseaseInfo']['TunnelID'])) {
             return $error['error'] = 'There is no \'DiseaseInfo => TunnelID\'';
-        }elseif (!isset($post['DiseaseInfo']['DiseaseType'])) {
-            return $error['error'] = 'There is no \'DiseaseInfo => DiseaseType\'';
         }elseif (!isset($post['DiseaseInfo']['FoundTime'])) {
             return $error['error'] = 'There is no \'DiseaseInfo => FoundTime\'';
-        }elseif (!isset($post['DiseaseInfo']['DiseaseID'])) {
-            return $error['error'] = 'There is no \'DiseaseInfo => DiseaseID\'';
         }else{
             $database = $post['DiseaseInfo']['TunnelID'];
-            switch ($post['DiseaseInfo']['DiseaseType']) {
-                case '0':                   //裂缝cracks
+            for($Diseasetype = 0; $Diseasetype <= 4; $Diseasetype ++){
+                switch ($Diseasetype) {
+                    case '0':                   //裂缝cracks
                         $type = 'crack_disease';
+                        $theDetail[$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $post['DiseaseInfo']['FoundTime']);
                         break;
                     case '1':                   //漏洞leaks
                         $type = 'leak_disease';
+                        $theDetail[$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $post['DiseaseInfo']['FoundTime']);
                         break;
                     case '2':                   //掉块drop
                         $type = 'drop_disease';
+                        $theDetail[$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $post['DiseaseInfo']['FoundTime']);
                         break;  
                     case '3':                   //划痕scratch
                         $type = 'scratch_disease';
+                        $theDetail[$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $post['DiseaseInfo']['FoundTime']);
                     break;
                     default:                    //异常exception
                         $type = 'exception_disease';
+                        $theDetail[$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $post['DiseaseInfo']['FoundTime']);
                         break;
+                }   
+            
             }
-            $where['DiseaseID'] = $post['DiseaseInfo']['DiseaseID'];
-            $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $post['DiseaseInfo']['FoundTime']);
-            if (count($theDetail) == 0) {
+            $time = 0;
+            foreach ($theDetail as $Diseasestype => $Diseases) {
+                foreach ($Diseases as $key => $value) {
+                    $theDetails[$time] = $value;
+                    $time++;
+                }
+            }
+            if (count($theDetails) == 0) {
                 return $error['error'] = 'can not find anything';
             }
-            return $theDetail[0];
+            return $theDetail;
         }
     	
     }
