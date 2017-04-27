d557e9f5f12e05b65f1f2322268ca1e4c807099b
Chen kejie <ckj493991608@qq.com>
2017-04-27 15:23:33 +0800

    修改scanslided存在的返回开始里程和结束里程字段的错误

--- app/model/scanSlided.php
+++ app/model/scanSlided.php
@@ -79,10 +79,9 @@ class scanSlided extends Model
             }
             $NULL[0] = NULL;
             $theDisease['DiseasesInfo'] = $resoult == NULL ? $NULL : $resoult;
-            $theDisease['StartMileage'] = $theDisease['DiseasesInfo'][0] == NULL ? $post['Mileage'] : $theDisease['DiseasesInfo'][0]->Info->DiseasePosition['Mileage'];
-            $theDisease['EndMileage'] = $theDisease['DiseasesInfo'][0] == NULL ? $post['Mileage'] + 20 : $theDisease['DiseasesInfo'][count($theDisease['DiseasesInfo'])-1]->Info->DiseasePosition['Mileage'];
+            $theDisease['StartMileage'] = $post['Mileage'];
+            $theDisease['EndMileage'] = $post['Mileage'] + 20;
         }
-
         return $theDisease;
     }
 }
