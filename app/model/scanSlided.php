7befcd38205ba747d78c9e3dbf390229ab5fb37a
Chen kejie <ckj493991608@qq.com>
2017-04-27 12:34:53 +0800

    修改scanslided接口以满足接口返回数据结构规范

--- app/model/scanSlided.php
+++ app/model/scanSlided.php
@@ -31,44 +31,48 @@ class scanSlided extends Model
         unset($where);
         if ($resoult != NULL) {
             foreach ($resoult as $diseasesNum => $diseaseSearch) {
-            $where['DiseaseID'] = $diseaseSearch->DiseaseID;
-            $Examination = $diseaseSearch->FoundTime;
-            $Diseasetype = $diseaseSearch->DiseaseType;
-            switch ($Diseasetype) {
-                case '0':                   //裂缝cracks
-                $type = 'crack_disease';
-                $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
-                unset($theDetail->DiseaseID);
-                break;
-                case '1':                   //漏洞leaks
-                $type = 'leak_disease';
-                $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
-                unset($theDetail->DiseaseID);
-                break;
-                case '2':                   //掉块drop
-                $type = 'drop_disease';
-                $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
-                unset($theDetail->DiseaseID);
-                break;  
-                case '3':                   //划痕scratch
-                $type = 'scratch_disease';
-                $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
-                unset($theDetail->DiseaseID);
-                break;
-                default:                    //异常exception
-                $type = 'exception_disease';
-                $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
-                unset($theDetail->DiseaseID);
-                break;
-            }   
-
-            $resoult[$diseasesNum]->DiseaseDetail = $theDetail;
+                $where['DiseaseID'] = $diseaseSearch->DiseaseID;
+                $Examination = $diseaseSearch->FoundTime;
+                $Diseasetype = $diseaseSearch->DiseaseType;
+                switch ($Diseasetype) {
+                    case '0':                   //裂缝cracks
+                    $type = 'crack_disease';
+                    $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
+                    unset($theDetail->DiseaseID);
+                    break;
+                    case '1':                   //漏洞leaks
+                    $type = 'leak_disease';
+                    $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
+                    unset($theDetail->DiseaseID);
+                    break;
+                    case '2':                   //掉块drop
+                    $type = 'drop_disease';
+                    $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
+                    unset($theDetail->DiseaseID);
+                    break;  
+                    case '3':                   //划痕scratch
+                    $type = 'scratch_disease';
+                    $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
+                    unset($theDetail->DiseaseID);
+                    break;
+                    default:                    //异常exception
+                    $type = 'exception_disease';
+                    $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
+                    unset($theDetail->DiseaseID);
+                    break;
+                }   
+                $resoult[$diseasesNum]->PNGURL = $theDetail->PNGFile;
+                $theDetail->DiseasePosition['Mileage'] = $resoult[$diseasesNum]->Mileage;
+                $theDetail->DiseasePosition['Position'] = $resoult[$diseasesNum]->Position; 
+                $resoult[$diseasesNum]->Info = $theDetail;
+                unset($theDetail->PNGFile);
+                unset($resoult[$diseasesNum]->Position);
+                unset($resoult[$diseasesNum]->Mileage);
+            }
         }
-        }
-        
         $theDisease['DiseasesInfo'] = $resoult;
-        $theDisease['StartMileage'] = $theDisease['DiseasesInfo'] == NULL ? $post['Mileage'] : $theDisease['DiseasesInfo'][0]->Mileage;
-        $theDisease['EndMileage'] = $theDisease['DiseasesInfo'] == NULL ? $post['Mileage'] + 20 : $theDisease['DiseasesInfo'][count($theDisease['DiseasesInfo'])-1]->Mileage;
+        $theDisease['StartMileage'] = $theDisease['DiseasesInfo'] == NULL ? $post['Mileage'] : $theDisease['DiseasesInfo'][0]->Info->DiseasePosition['Mileage'];
+        $theDisease['EndMileage'] = $theDisease['DiseasesInfo'] == NULL ? $post['Mileage'] + 20 : $theDisease['DiseasesInfo'][count($theDisease['DiseasesInfo'])-1]->Info->DiseasePosition['Mileage'];
         return $theDisease;
 
     }
