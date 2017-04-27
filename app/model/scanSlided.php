5223c73f7d590c18a7a7f86f7da308e9f025f121
Chen kejie <ckj493991608@qq.com>
2017-04-27 11:36:47 +0800

    修改ButtonOkClick的数据结构，将病害详细信息添加到病害详细字段中

--- app/model/scanSlided.php
+++ app/model/scanSlided.php
@@ -12,10 +12,10 @@ class scanSlided extends Model
     }
 
     public function searchTheDisease($post){
-       	$database = $post['TunnelInfo']['TunnelId'];
-    	$where['col'] = 'Mileage';
-    	$where['start'] = $post['Mileage'];
-    	$where['range'] = 20;
+        $database = $post['TunnelInfo']['TunnelId'];
+        $where['col'] = 'Mileage';
+        $where['start'] = $post['Mileage'];
+        $where['range'] = 20;
         foreach ($post['TunnelInfo']['ExaminationTime'] as $key => $value) {
             $whereCol['FoundTime'] = $value;
             $theDiseases[$key] = $this->theDatas->rangeSearch($database, 'disease', $where, '', $whereCol);
@@ -23,13 +23,50 @@ class scanSlided extends Model
         $resoultNum = 0;
         $resoult = NULL;
         foreach ($theDiseases as $time => $diseases) {
-           $resoult[$resoultNum] = $diseases;
+            foreach ($diseases as $key => $value) {
+                $resoult[$resoultNum] = $value;
+                $resoultNum ++;
+            }
         }
-    	
-    	$theDisease['DiseasesInfo'] = $resoult;
-    	$theDisease['StartMileage'] = $theDisease['DiseasesInfo'] == NULL ? $post['Mileage'] : $theDisease['DiseasesInfo'][0]->Mileage;
-    	$theDisease['EndMileage'] = $theDisease['DiseasesInfo'] == NULL ? $post['Mileage'] + 20 : $theDisease['DiseasesInfo'][count($theDisease['DiseasesInfo'])-1]->Mileage;
-    	return $theDisease;
-        
+        unset($where);
+        foreach ($resoult as $diseasesNum => $diseaseSearch) {
+            $where['DiseaseID'] = $diseaseSearch->DiseaseID;
+            $Examination = $diseaseSearch->FoundTime;
+            $Diseasetype = $diseaseSearch->DiseaseType;
+            switch ($Diseasetype) {
+                case '0':                   //裂缝cracks
+                $type = 'crack_disease';
+                $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
+                unset($theDetail->DiseaseID);
+                break;
+                case '1':                   //漏洞leaks
+                $type = 'leak_disease';
+                $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
+                unset($theDetail->DiseaseID);
+                break;
+                case '2':                   //掉块drop
+                $type = 'drop_disease';
+                $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
+                unset($theDetail->DiseaseID);
+                break;  
+                case '3':                   //划痕scratch
+                $type = 'scratch_disease';
+                $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
+                unset($theDetail->DiseaseID);
+                break;
+                default:                    //异常exception
+                $type = 'exception_disease';
+                $theDetail = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, $where, $Examination)[0];
+                unset($theDetail->DiseaseID);
+                break;
+            }   
+
+            $resoult[$diseasesNum]->DiseaseDetail = $theDetail;
+        }
+        $theDisease['DiseasesInfo'] = $resoult;
+        $theDisease['StartMileage'] = $theDisease['DiseasesInfo'] == NULL ? $post['Mileage'] : $theDisease['DiseasesInfo'][0]->Mileage;
+        $theDisease['EndMileage'] = $theDisease['DiseasesInfo'] == NULL ? $post['Mileage'] + 20 : $theDisease['DiseasesInfo'][count($theDisease['DiseasesInfo'])-1]->Mileage;
+        return $theDisease;
+
     }
 }
