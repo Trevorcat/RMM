9e724c74bf573c9e2f1ce657de5c8f99a442dbb5
Chen kejie <ckj493991608@qq.com>
2017-04-28 10:12:38 +0800

    修改ItemHold传入参数的结构

--- app/model/itemHold.php
+++ app/model/itemHold.php
@@ -14,33 +14,32 @@ class itemHold extends Model
     public function getTheDetail($post){
         $database = $post['TunnelInfo']['TunnelId'];
         $ExaminationTime = $post['DiseaseInfo']['FoundTime'];
-        foreach ($ExaminationTime as $time => $examination) {
             for($Diseasetype = 0; $Diseasetype <= 4; $Diseasetype ++){
                 switch ($Diseasetype) {
                         case '0':                   //裂缝cracks
                         $type = 'crack_disease';
-                        $theDetail[$time][$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $examination);
+                        $theDetail[$time][$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $ExaminationTime);
                         break;
                         case '1':                   //漏洞leaks
                         $type = 'leak_disease';
-                        $theDetail[$time][$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $examination);
+                        $theDetail[$time][$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $ExaminationTime);
                         break;
                         case '2':                   //掉块drop
                         $type = 'drop_disease';
-                        $theDetail[$time][$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $examination);
+                        $theDetail[$time][$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $ExaminationTime);
                         break;  
                         case '3':                   //划痕scratch
                         $type = 'scratch_disease';
-                        $theDetail[$time][$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $examination);
+                        $theDetail[$time][$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $ExaminationTime);
                         break;
                         default:                    //异常exception
                         $type = 'exception_disease';
-                        $theDetail[$time][$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $examination);
+                        $theDetail[$time][$Diseasetype] = $this->theDatas->getDataByTablenameAndDatabasename($database, $type, '', $ExaminationTime);
                         break;
                     }   
                     
                 }
-            }
+            
             $time = 0;
             foreach ($theDetail as $Diseasestype => $Diseases) {
                 foreach ($Diseases as $Diseasetype => $disease) {
